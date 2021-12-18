9440595022218106ef54b17bcadd3477e6f23d98
KevCodes <41352514+kevman5@users.noreply.github.com>
2021-12-18 15:46:38 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace Tangential_Velocity
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tb_1 = new System.Windows.Forms.TextBox();
+            this.tb_2 = new System.Windows.Forms.TextBox();
+            this.tb_3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label1.Font = new System.Drawing.Font("Myanmar Text", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(144, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Radius(r)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label2.Font = new System.Drawing.Font("Myanmar Text", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(144, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(151, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Angular Velocity(w)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label3.Font = new System.Drawing.Font("Myanmar Text", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(144, 197);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(167, 31);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tangential Velocity(v):";
+            // 
+            // tb_1
+            // 
+            this.tb_1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.tb_1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.tb_1.Font = new System.Drawing.Font("Myanmar Text", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_1.Location = new System.Drawing.Point(333, 42);
+            this.tb_1.Name = "tb_1";
+            this.tb_1.Size = new System.Drawing.Size(100, 37);
+            this.tb_1.TabIndex = 3;
+            // 
+            // tb_2
+            // 
+            this.tb_2.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.tb_2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.tb_2.Font = new System.Drawing.Font("Myanmar Text", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_2.Location = new System.Drawing.Point(333, 85);
+            this.tb_2.Name = "tb_2";
+            this.tb_2.Size = new System.Drawing.Size(100, 37);
+            this.tb_2.TabIndex = 4;
+            // 
+            // tb_3
+            // 
+            this.tb_3.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.tb_3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.tb_3.Font = new System.Drawing.Font("Myanmar Text", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_3.Location = new System.Drawing.Point(333, 197);
+            this.tb_3.Name = "tb_3";
+            this.tb_3.Size = new System.Drawing.Size(100, 37);
+            this.tb_3.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(236, 139);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 37);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Yellow;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(102, 8);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(387, 33);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Finding the Tangential Velocity";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tb_3);
+            this.Controls.Add(this.tb_2);
+            this.Controls.Add(this.tb_1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tb_1;
+        private System.Windows.Forms.TextBox tb_2;
+        private System.Windows.Forms.TextBox tb_3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tangential_Velocity
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double r1, w1, v1;
+            r1 = double.Parse(tb_1.Text);
+            w1 = double.Parse(tb_2.Text);
+            v1 = r1 * w1;
+            tb_3.Text = v1.ToString();
+        }
+    }
+}
